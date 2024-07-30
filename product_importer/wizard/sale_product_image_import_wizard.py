f2f55e5532734456bc5bbb187d85b8ca274bf7d5
jinye <jinye@domatix.com>
2024-07-30 23:41:06 +0200

    imágenes en variantes

--- product_importer/wizard/sale_product_image_import_wizard.py
+++ product_importer/wizard/sale_product_image_import_wizard.py
@@ -29,6 +29,11 @@ class ImportProductsWizard(models.TransientModel):
 
             for index, row in df.iterrows():
                 article_number = row['Article number short with point']
+                color_code = row['Image color number']
+                if color_code != '-':
+                    color_code = int(row['Image color number'])
+                else:
+                    color_code = ''
                 image_name = row['File Name']
                 image_path = None
                 try:
@@ -41,30 +46,47 @@ class ImportProductsWizard(models.TransientModel):
                 if not product:
                     print(f"Product with article number {article_number} not found.")
                     continue
+                color_product_variant_ids = product.product_variant_ids.filtered(
+                    lambda p: any(
+                        ptav.product_attribute_value_id.color_code == str(color_code) and ptav.attribute_id.name == "Color"
+                        for ptav in p.product_template_attribute_value_ids
+                    )
+                )
+                if color_product_variant_ids:
+                    for color_product_variant_id in color_product_variant_ids:
+                        try:
+                            with open(image_path, 'rb') as image_file:
+                                image_data = base64.b64encode(image_file.read())
+                        except IOError as e:
+                            print(f"Error: {e}")
+                            continue
 
-                existing_image = self.env['product.image'].search([('product_tmpl_id', '=', product.id), ('name', '=', image_name)], limit=1)
-                if existing_image:
-                    print(f"Image {image_name} already exists for product {product.name}, skipping...")
-                    continue
+                        color_product_variant_id.image_1920 = image_data
+                        self.env.cr.commit()
+                else:
+                    existing_image = self.env['product.image'].search([('product_tmpl_id', '=', product.id), ('name', '=', image_name)], limit=1)
+                    if existing_image:
+                        print(f"Image {image_name} already exists for product {product.name}, skipping...")
+                        continue
 
-                try:
-                    with open(image_path, 'rb') as image_file:
-                        image_data = base64.b64encode(image_file.read())
-                except IOError as e:
-                    print(f"Error: {e}")
-                    continue
+                    try:
+                        with open(image_path, 'rb') as image_file:
+                            image_data = base64.b64encode(image_file.read())
+                    except IOError as e:
+                        print(f"Error: {e}")
+                        continue
 
-                new_image = self.env['product.image'].create({
-                    'product_tmpl_id': product.id,
-                    'image_1920': image_data,
-                    'name': image_name
-                })
-                self.env.cr.commit()
+                    new_image = self.env['product.image'].create({
+                        'product_tmpl_id': product.id,
+                        'image_1920': image_data,
+                        'name': image_name
+                    })
+                    self.env.cr.commit()
 
-                print(f"Imported image for product {product.name}")
+                    print(f"Imported image for product {product.name}")
 
-                if not first_image_assigned.get(article_number):
-                    product.image_1920 = new_image.image_1920
-                    first_image_assigned[article_number] = True
-                    print(f"Updated main image for {product.name}")
+                    if not first_image_assigned.get(article_number):
+                        product.image_1920 = new_image.image_1920
+                        first_image_assigned[article_number] = True
+                        print(f"Updated main image for {product.name}")
 
